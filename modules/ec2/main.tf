c9a732c0adc706e6b45a0864ff8353e551246a2c
sukesh <ssukesh54@gmail.com>
2021-07-16 13:55:11 +0530

    key pair added

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web-1" {
     ami = var.imagename
     instance_type = "t2.micro"
+    key_name = "awswebservice"
     subnet_id = var.subnetid
     vpc_security_group_ids = ["${var.sgid}"]
     associate_public_ip_address = true	
